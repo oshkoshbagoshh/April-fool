a3b00ff8b6606adc968c569a1fcc0254ae4a5e38
AJ Javadi <amirjavadi25@gmail.com>
2025-04-06 05:14:14 -0500

    updated frontend assets with icons , bulma, favicons

--- public/index.php
+++ public/index.php
@@ -1,6 +1,95 @@
 <?php
 
-echo "hello, friend";
+//echo "hello, friend";
 
 
+// TODO:  import page specific templates, assets, icons, etc
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/scss/april-fool.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="./assets/img/icons/favicon/favicon.ico">
+
+</head>
+
+<body>
+<!-- TOP / HEADER -->
+<!-- Navbar / Menus -->
+<!-- END HEADER -->
+
+<!-- HERO / LANDING -->
+<!--Hero section  / landing page -->
+<!-- FULL WIDTH OF VIEWPORT -->
+<!--    Import overlay of SVG graphic or hero -->
+<!--CALL TO ACTIION -->\
+
+<!-- END HERO -->
+
+<!--SERVICES  -->
+
+<!--TFN Media Services -->
+<!--        Cards with info  -->
+<!--            Icons -->
+<!-- Partnerships -->
+
+<!--- MAYBE:  Coming Soon - TFN Music -->
+
+
+<!-- END SERVICES -->
+
+
+<!--TESTIMONIALS / WINS  / CASE STUDIES -->
+<!--    Carousel / slideshow -->
+<!---->
+
+<!--END TESTIMONIALS -->
+
+<!-- ABOUT US  -->
+<!-- About the Company   -->
+<!--  About the Team  -->
+<!-- ABOUT NATHAN   -->
+
+<!-- END ABOUT US -->
+
+
+<!-- FAQ -->
+<!-- details that pop up on click -->
+
+<!-- END FAQ-->
+
+<!-- CONTACT / CONVERSION -->
+<!-- Book an appointment / calendly embed -->
+<!-- Contact Form : need AJAX or PHP  to send this to SMTP server / save in database -->
+<!-- anything else for  call to action -->
+<!-- Track with analytics -->
+
+<!-- Subscribe to Blog? -->
+
+<!--Blog -->
+<!-- show featured / latest posts -->
+<!-- Blog as separate page -->
+
+<!-- each post type / article needs to be using a "blogpost.php" template  -->
+
+
+<!-- Footer -->
+<footer>
+    <!-- Legal / Info / Privacy Policy etc -->
+    <!--     Scripts -->
+    <!--   Other footer stuff   -->
+
+    <script type="text/javascript" src="assets/js/script.js">
+</footer>
+
+
+    </body>
+    </html>
 
